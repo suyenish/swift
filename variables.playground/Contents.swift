578eeee97d054da3271428ff2ba91f786bdbfbee
Daulet or Dan <83438504+suyenish@users.noreply.github.com>
2021-11-30 10:30:11 +0900

    Add files via upload

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var myAge = 19
+
+myAge = 20
+
+var surname = "Suienish"
+
+let myName = "Daulet"
+
+print(greeting)
+
+print(myAge)
+
+print(21)
+
+print(myName , myAge)
+
+print(surname + " " + myName)
