021214dc18e510a857cde5b2c2a996e04ea98e65
Daulet or Dan <83438504+suyenish@users.noreply.github.com>
2021-11-30 10:29:18 +0900

    Add files via upload

---
+++ dataTypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var x = 20
+
+let const = 7
+
+var x1 : Int = 13
+
+var x2 : Double = 6.2
+
+var x3 : Float = 3.377777
+
+var sum = x1 + Int(x2)
+
+typealias myNumber = Int
+
+var y : myNumber = 6
+
+print(y)
+
+var doter : Bool = true
+if doter == true {
+    print("Bye!")
+} else {
+    print("Hello!")
+}
+
+var zp = 10_000_000
